a216e6e9b32d5db464698cf6e66008f9866fc6cb
tsrigo <717660037@qq.com>
2022-09-21 22:15:02 +0800

    [done] task2-A

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,31 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    # addi sp, sp, -8
+    # sw a0, 4(sp)
+    # sw a1, 0(sp)
+    ebreak
+    ble a1, x0, error
+    addi t1, x0, 0      # t1: count the location of the array.
 loop_start:
-
-
-
-
-
-
-
+    bge t1, a1, loop_end# if t1 >= array.length, goto loop_end.
 
 loop_continue:
-
-
-
+    slli t2, t1, 2      # t2: offset of the array, because of type of int we use slli.
+    addi t1, t1, 1      # update the conter.
+    add t0, t2, a0      # t0: address of a[a1 + t1].
+    lw t3, 0(t0)        # t3: content of a[a1 + t1].
+    bge t3, x0, loop_start # if a[a1 + t1] >= 0, loop again
+    sw x0, 0(t0)           # else it should be assigned the 0
+    bge x0, x0, loop_start # loop again
 loop_end:
 
-
     # Epilogue
-
-
+    # lw a0, 4(sp)
+    # lw a1, 0(sp)
+    # addi sp, sp, 8
     jr ra
+
+error:
+    li a0 36
+    j exit
