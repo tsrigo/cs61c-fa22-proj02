69e66c82c10380655903635f7f23eb134360faa6
tsrigo <717660037@qq.com>
2022-09-21 23:04:40 +0800

    [done] task4-A

--- src/dot.s
+++ src/dot.s
@@ -20,25 +20,43 @@
 dot:
 
     # Prologue
+    ble a2, x0, error1
+    ble a3, x0, error2
+    ble a4, x0, error2
 
-
+    addi t1, x0, 0
+    addi t6, x0, 0
+    ebreak
 loop_start:
+    bge t1, a2, loop_end
 
+    mul t2, t1, a3
+    slli t2, t2, 2
+    add t0, t2, a0
+    lw t3, 0(t0)
 
+    mul t4, t1, a4
+    slli t4, t4, 2
+    add t0, t4, a1
+    lw t4, 0(t0)
 
+    mul t5, t3, t4
+    add t6, t6, t5
 
-
-
-
-
-
-
-
+    addi t1, t1, 1  
+    bge x0, x0, loop_start
 
 loop_end:
-
+    add a0, x0, t6
 
     # Epilogue
 
 
     jr ra
+
+error1:
+    li a0, 36
+    j exit
+error2:
+    li a0, 37
+    j exit
