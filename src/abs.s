5de0f9300e74c1089aab12c34094551120e116b7
tsrigo <717660037@qq.com>
2022-09-21 20:59:55 +0800

    [proj2] task1 of partA finished.

--- src/abs.s
+++ src/abs.s
@@ -13,6 +13,19 @@ abs:
 
     # PASTE HERE
 
+    # Load number from memory
+    ebreak
+    lw t0 0(a0)
+    bge t0, zero, done
+
+    # Negate a0
+    sub t0, x0, t0
+
+    # Store number back to memory
+    sw t0 0(a0)
+
+    done:
+    ret   
     # Epilogue
 
     jr ra
